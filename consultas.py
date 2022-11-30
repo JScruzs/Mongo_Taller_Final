726c239e1c5748e32decc43f5b1d30060d5c2752
Jscruzs <jscruz234@misena.edu.co>
2022-11-30 09:29:23 -0500

    Se crea el menu con un ciclo while con su respectiva bandera

--- consultas.py
+++ consultas.py
@@ -20,3 +20,32 @@ print("7. Cuantos casos fueron por personas del extranjero")
 print("8. Cuantos casos se registraron en Antioquía, menores de 30 años y por persona extranjeras")
 print("9. Cuantos casos se registraron por personas de España")
 print("10. Cuantos casos se registraron por personas de edad entre 20 y 50 años")
+
+# Se crea la bandera para el ciclo while.
+
+cuestion = True
+
+# Se crea el menu para escoger las opciones.
+
+while cuestion:  
+    query = int(input("Seleccione la opcion que desea consultar"))
+    if query == 1:
+        fuction_one = collection.count_documents({})
+    if query == 2:
+        function_two()
+    if query == 3:
+        function_three()
+    if query == 4:
+        function_four()
+    if query == 5:
+        function_five()
+    if query == 6:
+        function_six()
+    if query == 7:
+        function_seven()
+    if query == 8:
+        function_eigth()
+    if query == 9:
+        function_nine()
+    if query == 10:
+        function_ten()
